a53f6c0dca3782fb0487e89f8e3ce9053e7d5509
flexion <github.flx.account@flexion.ch>
2025-10-02 23:17:32 +0200

    big endian colors

--- RSDKv4/Renderer.cpp
+++ RSDKv4/Renderer.cpp
@@ -642,7 +642,11 @@ int LoadTexture(const char *filePath, int format)
                     }
 
 #if RETRO_USING_OPENGL
+#if defined(__sgi) // if big endian
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->width, texture->height, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, pixels);
+#else
                     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->width, texture->height, 0, GL_RGBA, GL_UNSIGNED_SHORT_4_4_4_4, pixels);
+#endif
                     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
                     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
                     glBindTexture(GL_TEXTURE_2D, 0);
@@ -777,7 +781,11 @@ void ReplaceTexture(const char *filePath, int texID)
                         }
                     }
 #if RETRO_USING_OPENGL
+#if defined(__sgi) // if big endian
+                    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, texture->width, texture->height, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, pixels);
+#else
                     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, texture->width, texture->height, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, pixels);
+#endif
                     glBindTexture(GL_TEXTURE_2D, 0);
 #endif
 
@@ -1068,8 +1076,11 @@ void TransferRetroBuffer()
             texBufferPtr += GFX_LINESIZE;
             frameBufferPtr += GFX_LINESIZE;
         }
-
+#if defined(__sgi) // if big endian
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, GFX_LINESIZE, SCREEN_YSIZE, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, Engine.texBuffer);
+#else
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, GFX_LINESIZE, SCREEN_YSIZE, GL_RGBA, GL_UNSIGNED_BYTE, Engine.texBuffer);
+#endif
     }
     else {
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, GFX_LINESIZE, SCREEN_YSIZE, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, Engine.frameBuffer);
