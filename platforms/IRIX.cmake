1ca2cede5f504cad44c369bfccfd4df5519ced8e
flexion <github.flx.account@flexion.ch>
2025-10-02 22:52:47 +0200

    irix cmake, linux remove glew

--- platforms/IRIX.cmake
+++ platforms/IRIX.cmake
@@ -24,7 +24,7 @@ if(NOT VORBIS_FOUND)
     message(NOTICE "libvorbis not found, attempting to build from source")
 else()
     message("found libvorbis")
-    target_link_libraries(RetroEngine -lGL ${VORBIS_STATIC_LIBRARIES})
+    target_link_libraries(RetroEngine ${VORBIS_STATIC_LIBRARIES})
     target_link_options(RetroEngine PRIVATE ${VORBIS_STATIC_LDLIBS_OTHER})
     target_compile_options(RetroEngine PRIVATE ${VORBIS_STATIC_CFLAGS})
 endif()
@@ -45,10 +45,16 @@ if(RETRO_USE_HW_RENDER)
 endif()
 
 if(RETRO_SDL_VERSION STREQUAL "2")
+    message(STATUS "-- Checking for SDL2 on IRIX --")
     pkg_check_modules(SDL2 sdl2 REQUIRED)
-    target_link_libraries(RetroEngine ${SDL2_STATIC_LIBRARIES})
-    target_link_options(RetroEngine PRIVATE ${SDL2_STATIC_LDLIBS_OTHER})
-    target_compile_options(RetroEngine PRIVATE ${SDL2_STATIC_CFLAGS})
+#    target_link_libraries(RetroEngine ${SDL2_STATIC_LIBRARIES})
+    target_link_libraries(RetroEngine /usr/lib32/libX11.so -lGL -lGLcore -lm ${SDL2_STATIC_LIBRARIES} )
+
+    target_link_options(RetroEngine PRIVATE ${SDL2_STATIC_LDLIBS_OTHER} /usr/lib32/libX11.so.1 /usr/lib32/libXext.a /usr/lib32/libXt.a /usr/lib32/libXm.so.1 /usr/lib32/libXpm.so.1 -Wl,--allow-shlib-undefined -Wl,-rpath-link=/usr/lib32 -Wl,-rpath=/usr/lib32:/usr/sgug/lib32)
+
+    target_compile_options(RetroEngine PRIVATE ${SDL2_STATIC_CFLAGS} -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include -I/usr/include -I/usr/sgug/include)
+
+
 elseif(RETRO_SDL_VERSION STREQUAL "1")
     pkg_check_modules(SDL1 sdl1 REQUIRED)
     target_link_libraries(RetroEngine ${SDL1_STATIC_LIBRARIES})
