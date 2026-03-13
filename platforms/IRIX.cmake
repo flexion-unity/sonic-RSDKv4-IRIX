839f6cf22846b21810dd86bb3e40f30a0ae022ae
flexion <flx@flexion.ch>
2026-03-13 13:32:55 +0000

    IRIX cmake with rpath for local SDL2 GL lib

--- platforms/IRIX.cmake
+++ platforms/IRIX.cmake
@@ -12,7 +12,7 @@ if(NOT OGG_FOUND)
     message(NOTICE "libogg not found, attempting to build from source")
 else()
     message("found libogg")
-    target_link_libraries(RetroEngine ${OGG_STATIC_LIBRARIES})
+    target_link_libraries(RetroEngine PRIVATE ${OGG_STATIC_LIBRARIES})
     target_link_options(RetroEngine PRIVATE ${OGG_STATIC_LDLIBS_OTHER})
     target_compile_options(RetroEngine PRIVATE ${OGG_STATIC_CFLAGS})
 endif()
@@ -24,7 +24,7 @@ if(NOT VORBIS_FOUND)
     message(NOTICE "libvorbis not found, attempting to build from source")
 else()
     message("found libvorbis")
-    target_link_libraries(RetroEngine ${VORBIS_STATIC_LIBRARIES})
+    target_link_libraries(RetroEngine PRIVATE ${VORBIS_STATIC_LIBRARIES})
     target_link_options(RetroEngine PRIVATE ${VORBIS_STATIC_LDLIBS_OTHER})
     target_compile_options(RetroEngine PRIVATE ${VORBIS_STATIC_CFLAGS})
 endif()
@@ -38,7 +38,7 @@ if(RETRO_USE_HW_RENDER)
 
 #    else()
 #        message("found GLEW")
-#        target_link_libraries(RetroEngine ${GLEW_STATIC_LIBRARIES})
+#        target_link_libraries(RetroEngine PRIVATE ${GLEW_STATIC_LIBRARIES})
 #        target_link_options(RetroEngine PRIVATE ${GLEW_STATIC_LDLIBS_OTHER})
 #        target_compile_options(RetroEngine PRIVATE ${GLEW_STATIC_CFLAGS})
 #    endif()
@@ -46,18 +46,68 @@ endif()
 
 if(RETRO_SDL_VERSION STREQUAL "2")
     message(STATUS "-- Checking for SDL2 on IRIX --")
-    pkg_check_modules(SDL2 sdl2 REQUIRED)
+#    pkg_check_modules(SDL2 sdl2 REQUIRED)
+#    find_library(MYSDL2LIB NAMES SDL2 PATHS ${CMAKE_SOURCE_DIR}/lib)
 
 
-    target_link_libraries(RetroEngine  -Wl,-rpath-link=/usr/lib32 -Wl,-rpath=/usr/lib32:/usr/sgug/lib32 -lGLcore -laudio -Wl,--allow-shlib-undefined -pthread ${SDL2_STATIC_LIBRARIES} )
+# For IRIX with SGUG-RSE we cannot use the default SDL2 library due to missing GL support
+# use rpath to link to ./lib respectively ../lib/ with the SDL2-GL library included in this bundle
+# (you will probably still need sdl2-dev for headers)
 
-    target_compile_options(RetroEngine PRIVATE ${SDL2_STATIC_CFLAGS} -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include -I/usr/include -I/usr/sgug/include )
 
-# for drmadison SDL2-GL rpm use this:
-# target_link_libraries(RetroEngine /usr/lib32/libX11.so -lGL -lGLcore -lm ${SDL2_STATIC_LIBRARIES} )
-# target_link_options(RetroEngine PRIVATE ${SDL2_STATIC_LDLIBS_OTHER}-lSDL2 -Wl,-rpath-link=/usr/lib32 -Wl,-rpath=/usr/lib32:/usr/sgug/lib32 -lGLcore -laudio -Wl,--allow-shlib-undefined -pthread )
-# target_compile_options(RetroEngine PRIVATE ${SDL2_STATIC_CFLAGS} -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include -I/usr/include -I/usr/sgug/include)
+#  set(MYSDL2LIB "/usr/sgug-flx/lib32/libSDL2-2.0.so.0")
+#  set(MYSDL2LIB "${CMAKE_SOURCE_DIR}/lib/libSDL2-2.0.so.0")
 
+# 1. Priority 1: Check /usr/sgug-flx/lib32/
+set(SGUGFLX_SDL2 "/usr/sgug-flx/lib32/libSDL2-2.0.so.0")
+if(EXISTS "${SGUGFLX_SDL2}")
+    set(MYSDL2LIB "${SGUGFLX_SDL2}")
+# 2. Priority 2: Fallback to ./lib/
+elseif(EXISTS "${CMAKE_SOURCE_DIR}/lib/libSDL2-2.0.so.0")
+    set(MYSDL2LIB "${CMAKE_SOURCE_DIR}/lib/libSDL2-2.0.so.0")
+else()
+    message(FATAL_ERROR "SDL2 not found in /usr/sgug-flx/lib32 OR ${CMAKE_SOURCE_DIR}/lib")
+endif()
+
+
+
+
+  message(STATUS "Using SDL2: ${MYSDL2LIB}") 
+
+  target_link_libraries(RetroEngine PRIVATE "${MYSDL2LIB}")
+
+    set_target_properties(RetroEngine PROPERTIES
+      BUILD_RPATH "\$ORIGIN/../lib"
+    )
+
+    target_link_libraries(RetroEngine PRIVATE
+     GLcore
+     audio
+     pthread
+    )
+
+    target_compile_options(RetroEngine PRIVATE
+      -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include
+      -I/usr/include
+      -I/usr/sgug/include
+      -I/usr/sgug/include/SDL2
+    )
+
+    target_link_options(RetroEngine PRIVATE
+      -Wl,--disable-new-dtags
+      -Wl,-rpath,$ORIGIN/lib     # ONLY your lib directory
+      -Wl,--allow-shlib-undefined
+      -Wl,-rpath-link=/usr/lib32
+      -Wl,-rpath=/usr/sgug-flx/lib32:./lib:../lib:/usr/lib32:/usr/sgug/lib32
+    )
+
+    target_compile_options(RetroEngine PRIVATE
+     -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include
+     -I/usr/include
+     -I/usr/sgug/include
+    )
+ 
+ 
 
 elseif(RETRO_SDL_VERSION STREQUAL "1")
     pkg_check_modules(SDL1 sdl1 REQUIRED)
